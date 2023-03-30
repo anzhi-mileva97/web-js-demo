496be21e0971c919648241dfb7e395b221ef7e03
Anzhi Mileva <anplmi97@gmail.com>
2023-03-30 12:57:18 +0300

    last refactor

--- demo/2-lodash vs underscore.js
+++ demo/2-lodash vs underscore.js
@@ -14,10 +14,3 @@ const array = u.map(_.keys(object), (key) => ({key, value: object[key]}));
 
 console.log(array);
 
-/*
-Lodash's map function can directly iterate over an object'skey-value pairs,
-while Underscore.js's map function requires first extracting the keys of the object using keys.
- This is a minor difference, but it highlights the fact that the APIs of Lodash and Underscore.js can
- differ in subtle ways.
- */
-
